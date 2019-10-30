62194dd39045391ffa96db21b5a6778bb82550b5
violetms <violet@sch.lv>
2019-10-30 15:14:52 +0800

    new file

---
+++ hostinfo.c
@@ -0,0 +1,45 @@
+/*
+ * 域名和它相关的IP地址之间的映射
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#include "wdebug.h"
+
+#define	 BUFSIZE	(1024)
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		P_MSG("Usage:\t%s <domain name>\n", argv[0]);
+		return -1;
+	}
+
+	struct addrinfo *rp, *result, hints;
+	char buf[BUFSIZE];
+	int rc = 0;
+       	int flags;
+	
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ((rc = getaddrinfo(argv[1], NULL, &hints, &result)) != 0) {
+		fprintf(stderr, "getaddrinfo error:\t%s\n", gai_strerror(rc));
+		return -1;
+	}
+
+	flags = NI_NUMERICHOST;	/*显示IP地址*/
+	for (rp=result; rp!=NULL;rp=rp->ai_next) {
+		getnameinfo(rp->ai_addr, rp->ai_addrlen, buf, BUFSIZE, NULL, 0, flags);
+		printf("%s\t%s\n", argv[1], buf);
+	}
+
+	freeaddrinfo(result);
+
+	return 0;
+}
